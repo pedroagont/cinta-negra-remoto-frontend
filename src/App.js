bb1424c9c76b5f0c730375a2415a61d93e4c6cfb
Pedro Gonzalez <pgonzalez@post.com>
2020-05-16 04:18:33 -0500

    Importar vistas y componentes, y configurar react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+// Views
+import Home from './views/Home';
+import Login from './views/Login';
+import Signup from './views/Signup';
+
+// Components
+import Navigation from './components/Navigation';
+
+// Misc
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <React.Fragment>
-      <h1>Hola desde App</h1>
-      <p>Lorem ipsum dolor sit amet</p>
-    </React.Fragment>
+    <Fragment>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
